8567dcae1d5a0d0f144bf15b438e75616847565d
unknown <sebderhy@gmail.com>
2019-10-21 00:18:18 +0300

    First version of the app, working with DenseNet121

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # derotate
 Deep learning API that understands if/how an image is rotated and "derotates" it
+
+## Model training
+
+The Notebook responsible for model training is [TODO]()
+
+## Deployment
+The web app that does the inference is based on the starter pack available in [TODO]()
+
+Production ready starter pack for creating fast responsive Web App for Fast.AI Image models using Starlette.io framework with Uvicorn ASGI server.
+
+Everything packaged in docker with requirement.txt, so you can push it to any docker hosted cloud service. Enjoy :)
+
+Few dockers hosted services where this starter pack will work =>
+
+https://zeit.co/now
+https://azure.microsoft.com/en-us/services/app-service/containers/
+https://getcarina.com/
+https://sloppy.io/en/
+https://giantswarm.io
+https://aws.amazon.com/ecs/
+https://cloud.google.com/cloud-build/docs/
+https://www.digitalocean.com/products/one-click-apps/docker/
---
+++ app/server.py
@@ -0,0 +1,86 @@
+import aiohttp
+import asyncio
+import uvicorn
+from fastai import *
+from fastai.vision import *
+from io import BytesIO
+from starlette.applications import Starlette
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import HTMLResponse, JSONResponse
+from starlette.staticfiles import StaticFiles
+import base64
+import numpy as np
+
+export_file_url = 'https://www.dropbox.com/s/cfmqp0sxfkr0gxc/isImgRotated-dn121-sz128.pkl?dl=1'
+# export_file_name = 'models/isImgRotated-rn18-sz128.pkl'
+export_file_name = 'models/isImgRotated-dn121-sz128.pkl'
+
+path = Path(__file__).parent
+
+app = Starlette()
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
+app.mount('/static', StaticFiles(directory='app/static'))
+
+
+async def download_file(url, dest):
+    if dest.exists(): return
+    print('Downloading model. This may take a 5 to 10 minutes...')
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.read()
+            with open(dest, 'wb') as f:
+                f.write(data)
+    print('Finished downloading! The app should now start')
+
+
+
+async def setup_learner():
+    await download_file(export_file_url, path / export_file_name)
+    try:
+        learn = load_learner(path, export_file_name)
+        return learn
+    except RuntimeError as e:
+        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+            print(e)
+            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+            raise RuntimeError(message)
+        else:
+            raise
+
+
+loop = asyncio.get_event_loop()
+tasks = [asyncio.ensure_future(setup_learner())]
+learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
+loop.close()
+
+
+@app.route('/')
+async def homepage(request):
+    html_file = path / 'view' / 'index.html'
+    return HTMLResponse(html_file.open().read())
+
+
+@app.route('/analyze', methods=['POST'])
+async def analyze(request):
+    img_data = await request.form()
+    img_bytes = await (img_data['file'].read())
+    img = open_image(BytesIO(img_bytes))
+    prediction = learn.predict(img)[0]
+    pred_str = str(prediction)
+
+    angle_to_rotate = {
+        'rotated90': 270,
+        'rotated180': 180,
+        'rotated270': 90,
+        'straight': 0
+    }
+    if pred_str=='rotated90':
+        print("COUCOU")
+    img = img.rotate(angle_to_rotate[pred_str])
+    img.save('app/static/tmp.jpg')
+    return JSONResponse({'result': str(prediction)})
+
+
+if __name__ == '__main__':
+    if 'serve' in sys.argv:
+        uvicorn.run(app=app, host='0.0.0.0', port=8080, log_level="info")
